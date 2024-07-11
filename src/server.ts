a030fa6c63941e9c4c6fb9f84ebea83375fae9b3
HugoBianquini <hugobianqui@hotmail.com>
2024-07-10 23:06:31 -0300

    feat: setup prisma

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 const app = fastify();
-
-app.get("/test", () => {
-  return "Hello World";
-});
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running!");
